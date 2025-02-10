a6bd5c440da52e4a6f65ca1de5501825713ea890
Cernous <redtraitor9@gmail.com>
2025-02-10 11:41:31 -0500

    minimized thread

--- Assignment 1/thread.py
+++ Assignment 1/thread.py
@@ -28,8 +28,6 @@ class Thread(threading.Thread):
                 kwargs["time"] (float) : by default 2 seconds unless specified (poll rate or function run rate)
     '''
     super().__init__()
-    self.poll_time = None if "time" not in kwargs else kwargs.pop("time")
-    self.stop_event = Event()
     self.name = name
     self.partial_command = command
 
@@ -39,36 +37,16 @@ class Thread(threading.Thread):
     '''
     print(f"[{dt.datetime.now().strftime('%H:%M:%S')}]: ", f"{self.name} running")
     logger.info(" ".join([f"[{dt.datetime.now().strftime('%H:%M:%S')}]: ", f"{self.name} running"]))
-    self.stop_event.clear()
     out = None
-    while True:
-      if self.poll_time is not None and (type(self.poll_time) is float or type(self.poll_time) is int):   
-        try:
-          out = self.partial_command()
-        except Exception as e:
-          # made to prevent deadlocks ("release an unlocked lock (mutex)")
-          print(f"[{dt.datetime.now().strftime('%H:%M:%S')}]: ", self.name, str(e))
-          logger.info(" ".join([f"[{dt.datetime.now().strftime('%H:%M:%S')}]: ", self.name, str(e)]))
-        # SLEEP NO RUN
-        time.sleep(self.poll_time)
-      else:
-        # if no looping is required then just execute the command once 
-        out = self.partial_command()
-        self.stop_event.set()
-
-      if self.stop_event.is_set():
-        # if a stop event is set then finish the execution of the partial command and break from the loop 
-        # so that the thread can join without stalling
-        break
+    try:
+      out = self.partial_command()
+    except Exception as e:
+      # made to prevent deadlocks ("release an unlocked lock (mutex)")
+      print(f"[{dt.datetime.now().strftime('%H:%M:%S')}]: ", self.name, str(e))
+      logger.info(" ".join([f"[{dt.datetime.now().strftime('%H:%M:%S')}]: ", self.name, str(e)]))
     print(f"[{dt.datetime.now().strftime('%H:%M:%S')}]: ", f"{self.name} done running", out)
     logger.info(" ".join([f"[{dt.datetime.now().strftime('%H:%M:%S')}]: ", f"{self.name} done running", str(out)]))
     return out
-
-  def stop(self):
-    self.stop_event.set()
-
-  def is_Done(self):
-    return self.stop_event.is_set()
 
   def join(self, timeout: float = 2) -> None:
     print(f"[{dt.datetime.now().strftime('%H:%M:%S')}]: ", f"{self.name} joining main thread")
