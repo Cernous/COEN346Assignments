e15cae877c168bb515f21841691dcbcc39dd1287
Cernous <redtraitor9@gmail.com>
2025-03-30 21:41:14 -0400

    modification to the scheduler

--- Assignment 3/main.py
+++ Assignment 3/main.py
@@ -28,14 +28,12 @@ class Process:
     Status's are dependent on the threads properties and NOT its own property for authenticity
     so even if the process is being juggled around, the status cannot be manipulated outside of its own class
     """
-    def __init__(self, arrival_Time: int, service_Time: int, user:str, p_Num: int):
+    def __init__(self, arrival_Time: int, service_Time: int, p_Num: int):
         self.arrival_Time = arrival_Time
         self.service_Time = service_Time
-        self.user = user
-        self.name = f"{user}, Process {p_Num}"
+        self.name = f"Process {p_Num}"
         self.__Thread = Thread(self.name, time=self.service_Time)
         self.__status = "Created"
-        
 
     def start(self):
         self.__status = "Running"
@@ -57,60 +55,54 @@ if __name__ == "__main__":
     timeLine = 0        # Simulated TimeLine
     scheduler = True    # Active Scheduler
     readyQ = []         # Ready Queue
-    CPU:Process = None  # Current Process on the CPU if CPU is none, that means there is nothing running otherwise it is a Process
-    deadLine = 0        # CPU's deadline to be compared with the timeline
-    # while scheduler:
-    #     """
-    #     There is no wrong way of programming this but only one way to achieve the near same results as the what we have in the assignment
-    #     As long as the Processes havent been processed by the CPU or the ready queue is still not empty the scheduler will remain running
-    #     """
-    #     scheduler = len(Processes) != 0 or len(readyQ) != 0 or CPU is not None
-    #     remove_indices = []
-    #     # Creates the process depending on its arrival time
-    #     for process in Processes:
-    #         if process[2] == timeLine:
-    #             # Arrival Time ding ding
-    #             current = Process(process[2], process[3], f"User {process[0]}", process[1])
-    #             remove_indices.append(Processes.index(process))
-    #             print(f"Time {timeLine}, {current.name}, {current.status()}")
-    #             readyQ.append(current) # Makes it available for the CPU whenever it is the first element
-    #             # Makes sures that the time allocation remains dynamic on the number of present processes
-    #             if f"User {process[0]}" in users:
-    #                 users[f"User {process[0]}"].append(current.name)
-    #             else:
-    #                 users[f"User {process[0]}"] = [current.name]
+    CPU:Process = [None] * cores  # Current Process on the CPU if CPU is none, that means there is nothing running otherwise it is a Process
+    deadLine = [0] * cores        # CPU's deadline to be compared with the timeline
+    while scheduler:
+        """
+        There is no wrong way of programming this but only one way to achieve the near same results as the what we have in the assignment
+        As long as the Processes havent been processed by the CPU or the ready queue is still not empty the scheduler will remain running
+        """
+        scheduler = len(Processes) != 0 or len(readyQ) != 0 or None not in CPU
+        remove_indices = []
+        # Creates the process depending on its arrival time
+        for process in Processes:
+            if process[2]*1000 == timeLine:
+                # Arrival Time ding ding
+                current = Process(process[1]*1000, process[2]*1000, process[10])
+                remove_indices.append(Processes.index(process))
+                print(f"Time {timeLine}, {current.name}, {current.status()}")
+                readyQ.append(current) # Makes it available for the CPU whenever it is the first element
 
-    #     # Pop elements of the processes list as they are created
-    #     remove_indices.reverse()
-    #     for i in remove_indices:
-    #         Processes.pop(i)
+        # Pop elements of the processes list as they are created
+        remove_indices.reverse()
+        for i in remove_indices:
+            Processes.pop(i)
 
-    #     if CPU is not None:
-    #         # If a process is currently on the CPU
-    #         if deadLine == timeLine:
-    #             # CPU Deadline ding ding
-    #             CPU.stop()
-    #             print(f"Time {timeLine}, {CPU.name}, {CPU.status()}")
-    #             if CPU.service_Time > 0:
-    #                 # Put back at the end of the ready queue if necessary
-    #                 readyQ.append(CPU)
-    #             else:
-    #                 # Makes sures that the time allocation remains dynamic on the number of present processes
-    #                 users[CPU.user].remove(CPU.name)
-    #             CPU = None
+        if None not in CPU:
+            # If a process is currently on the CPU
+            if any([d <= timeLine for d in deadLine]):
+                # CPU Deadline ding ding
+                index = CPU.index(None)
+                CPU[index].stop()
+                print(f"Time {timeLine}, {CPU[index].name}, {CPU[index].status()}")
+                if CPU.service_Time > 0:
+                    # Put back at the end of the ready queue if necessary
+                    readyQ.append(CPU)
+                else:
+                    # Makes sures that the time allocation remains dynamic on the number of present processes
+                    pass
+                CPU = None
         
-    #     """
-    #     Putting the resume here helps with immediately assigning something instead of waiting another second
-    #     """
-    #     if len(readyQ) > 0 and CPU is None:
-    #         # If the CPU is waiting for a process to run while something still lurks on the ready queue
-    #         CPU = readyQ.pop(0)
-    #         CPU.start()
-    #         print(f"Time {timeLine}, {CPU.name}, {CPU.status()}")
-    #         timeallocated = int(timequant / (len(users.keys()) * len(users[CPU.user])))
-    #         deadLine = timeLine + (timeallocated if CPU.service_Time >= timeallocated else CPU.service_Time)
-    #         # Manipulate the remaining service time to sync with the thread
-    #         CPU.service_Time = CPU.service_Time - timeallocated if CPU.service_Time - timeallocated >= 0 else 0
+        """
+        Putting the resume here helps with immediately assigning something instead of waiting another second
+        """
+        if len(readyQ) > 0 and None in CPU:
+            # If the CPU is waiting for a process to run while something still lurks on the ready queue
+            index = CPU.index(None)
+            CPU[index] = readyQ.pop(0)
+            CPU[index].start()
+            print(f"Time {timeLine}, {CPU[index].name}, {CPU[index].status()}")
+            deadLine[index] = CPU[index].arrival_Time + CPU[index].service_Time
         
-    #     time.sleep(1)
-    #     timeLine +=1
+        time.sleep(0.001)
+        timeLine += 1
